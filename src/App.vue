7cacaecbeaef34ed3cbbec7e9620cb6bcaa4abb7
ShellBin <maoxiaobing@live.com>
2020-11-16 13:21:36 +0800

    以 vue 框架初始化项目

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <IndexPage/>
+  </div>
+</template>
+
+<script>
+import IndexPage from './components/IndexPage'
+
+export default {
+  name: 'App',
+  components: {
+    IndexPage
+  },
+  mounted() {
+    console.log('欢迎使用')
+  }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
