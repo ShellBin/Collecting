5f0fc78fdc5c9da6bee73d4eedcea57f6b6cc86c
ShellBin <maoxiaobing@live.com>
2020-11-19 14:24:02 +0800

    文件上传逻辑确定

--- server/index.js
+++ server/index.js
@@ -20,7 +20,6 @@ server.use('/api/v1/',router)
 // API: 获取当前的收集任务
 router.get('/fetchTask',indexPage.fetchTask)
 // API: 处理上传的文件
-// router.use('/uploadFile',indexPage.uploadFile)
 router.post('/uploadFile',indexPage.uploadFile)
 
 console.log('Server is running at '+config.http.port+' port')
--- server/indexPage/upload-file.js
+++ server/indexPage/upload-file.js
@@ -14,22 +14,23 @@ function indexStuInfo(stuId) {
         fullStuId = stuData[stuId].fullStuId
         return true
     } else return false
+}
 
+function packingRename() {
+    // TODO: 如果是单个文件，则重命名。如果是多个文件则压缩后重命名
 }
 
 function uploadFile (req, res) {
     if (indexStuInfo(req.headers.stuid)) {
+        packingRename()
         res.send({
             status: 'success',
-            stuName: stuName,
-            idCard: idCard,
-            fullStuId: fullStuId,
-            // TODO: 通过不同的 Params 分支处理。先查看是否是合法用户，返回 success 拉起上传开始
-            // TODO: 文件上传处理（判断大小，接受上传到指定文件夹并重命名），将学号后三位放在 header 里
+            // TODO: 文件上传处理（判断大小，接受上传到指定文件夹并重命名）
         })
     } else {
         res.status(403).send({
             status: 'error',
+            message: '用户不存在'
         })
     }
 }
