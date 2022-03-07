29c828d94e345601e11f7195672572857bd52a3b
ShellBin <maoxiaobing@live.com>
2022-03-07 16:08:18 +0800

    update

--- collecting_frontend/src/App.js
+++ collecting_frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import logo from './img/logo.jpg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App" id="App">
+        <div className="background" />
+        <div className="container">
+          <div className="header">
+            <div className="logo">
+              <span className="logo-text">Collecting</span>
+                <img src={logo} className="logo-image" alt="logo" />
+            </div>
+            <Button type="primary" className="admin-login">管理员登录</Button>
+          </div>
+          <div className="collecting-body">
+            <div className="">
+              提交任务
+
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
